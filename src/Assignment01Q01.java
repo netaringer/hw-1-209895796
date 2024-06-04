f6ee472f9691f27b017d58f7435ea90320901730
netaringer <netaringer@mail.tau.ac.il>
2024-06-04 23:53:28 +0300

    ascii_5_divisable

--- src/Assignment01Q01.java
+++ src/Assignment01Q01.java
@@ -3,7 +3,15 @@ public class Assignment01Q01 {
 
 	public static void main(String[] args) {
 		// *** your code goes here below ***
-		
-	}
-
+		Assignment01Q01.ascii_5_divisible(args);
+		}
+	public static void ascii_5_divisible(String[] st_array) {
+        for (String str : st_array) {
+                char ch = str.charAt(0);
+                int ascii = (int) ch;
+                if (ascii % 5 == 0) {
+                    System.out.println(ch);
+                }
+        	}
+		}
 }
